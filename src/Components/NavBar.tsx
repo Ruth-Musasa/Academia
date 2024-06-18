3cbe7fffcc9d39a8729f9063fd5c34eca9e947ca
Ruth-Musasa <ruthebondo2001@gmail.com>
2024-06-18 12:45:10 +0200

    creation de la navBar

--- src/Components/NavBar.tsx
+++ src/Components/NavBar.tsx
@@ -2,18 +2,18 @@ import { BrowserRouter as Router, Link } from "react-router-dom";
 
 export default function NavBar() {
     return (
-        <div className="flex justify-evenly text-base py-6 items-center">
+        <div className="flex justify-evenly text-base text-gray-500 py-6 items-center font-medium	">
             <img src="src/assets/img/Rectangle 2.svg" alt="Logo" />
             <div >
-                <Link to='/' className="px-6 py-3">Acceuil </Link>
-                <Link to='/' className="px-6 py-3">Cours</Link>
-                <Link to='/' className="px-6 py-3">Blogs</Link>
-                <Link to='/' className="px-6 py-3">Contacts</Link>
+                <Link to='/' className="px-6 py-3  hover:font-semibold hover:text-black">Acceuil </Link>
+                <Link to='/' className="px-6 py-3  hover:font-semibold hover:text-black">Cours</Link>
+                <Link to='/' className="px-6 py-3  hover:font-semibold hover:text-black">Blogs</Link>
+                <Link to='/' className="px-6 py-3  hover:font-semibold hover:text-black">Contacts</Link>
             </div>
-            <div className="flex ">
-                <div className="flex px-6 py-3">
+            <div className="flex items-center">
+                <div className="flex px-6 py-3 space-x-1.5">
                     <img src="src/assets/img/Lock.svg" alt="Lock" />
-                    <button>  Login</button>
+                    <button> Login</button>
                 </div>
                 <button className="px-6 py-3 bg-[#68226F] text-white rounded-lg	 "> Sign up for Free </button>
             </div>
